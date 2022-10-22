39c9ae59b6c6d6147ab20d628933c57fc492cab9
wyxather <wyxather@gmail.com>
2022-10-22 21:17:21 +0700

    Add print method to each name pool version.

--- UEDumper/Source/UEEngine.cpp
+++ UEDumper/Source/UEEngine.cpp
@@ -53,30 +53,27 @@ namespace
 		return (T)(address + sizeof(std::uint32_t) + *reinterpret_cast<std::uint32_t*>(address));
 	}
 
-	class UEObjectArray
-	{
-	public:
-	};
-
-	class UENamePool
-	{
-	public:
-	};
+	class UEObjectArray {};
+	class UENamePool {};
 
 	class UEObjectArray_4_27_2_0 : public UEObjectArray
 	{
 	public:
-		struct UObject {
+		struct UObject
+		{
 			char pad_0000[264]; //0x0000
+
 		}; //Size: 0x0108
 		static_assert(sizeof(UObject) == 0x108);
 
-		struct FUObjectItem {
+		struct FUObjectItem
+		{
 			UObject* object; //0x0000
 			std::int32_t flags; //0x0008
 			std::int32_t clusterRootIndex; //0x000C
 			std::int32_t serialNumber; //0x0010
 			char pad_0014[4]; //0x0014
+
 		}; //Size: 0x0018
 		static_assert(sizeof(FUObjectItem) == 0x18);
 
@@ -86,35 +83,109 @@ namespace
 		std::int32_t numElements; //0x0014
 		std::int32_t maxChunks; //0x0018
 		std::int32_t numChunks; //0x001C
+
 	}; //Size: 0x0020
 	static_assert(sizeof(UEObjectArray_4_27_2_0) == 0x20);
 
 	class UENamePool_4_27_2_0 : public UENamePool
 	{
 	public:
+		explicit UENamePool_4_27_2_0(const UEGame& game, std::string_view pattern, const std::ptrdiff_t offset) noexcept
+		{
+			std::memset(this, 0, sizeof(*this));
+			const auto names = relativeToAbsolute<decltype(this)>(findPattern(game.getImage(), pattern) + offset);
+			currentBlock = names->currentBlock;
+			currentByteCursor = names->currentByteCursor;
+			std::memcpy(blocks, names->blocks, names->currentBlock * sizeof(std::uintptr_t));
+		}
+
+		struct Block
+		{
+			std::byte* data;
+		};
+
+		auto print(const UEGame& game, const UEEngine& engine) noexcept
+		{
+			for (std::uint32_t i = 0; i < currentBlock; i++) {
+
+				for (std::uint32_t j = 0; j < currentByteCursor;) {
+
+					std::size_t info{};
+					if (!game.read(reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(blocks[i]) + j), &info, engine.FNameEntry.HeaderSize))
+						continue;
+
+					auto length = info >> engine.FNameEntry.LenBit;
+					const auto wide = (info >> engine.FNameEntry.WideBit) & 1;
+
+					j += engine.FNameEntry.HeaderSize;
+
+					std::unique_ptr<std::byte[]> name = std::make_unique<decltype(name)::element_type[]>(length + 1);
+					if (!game.read(reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(blocks[i]) + j), name.get(), length))
+						continue;
+
+					j += static_cast<decltype(j)>(length);
+					if (length % 2)
+						j++;
+
+					std::printf("%s[%zd]\n", reinterpret_cast<const char*>(name.get()), length);
+				}
+			}
+		}
+
 		std::byte lock[8]; //0x0000
 		std::uint32_t currentBlock; //0x0008
 		std::uint32_t currentByteCursor; //0x000C
-		struct { std::byte* data; } *blocks[8192]; //0x0010
+		Block *blocks[8192]; //0x0010
+
 	}; //Size: 0x10010
 	static_assert(sizeof(UENamePool_4_27_2_0) == 0x10010);
 
 	class UENamePool_4_20_3_0 : public UENamePool
 	{
 	public:
-		struct Entry {
+		explicit UENamePool_4_20_3_0(const UEGame& game, std::string_view pattern, const std::ptrdiff_t offset) noexcept
+		{
+			std::memset(this, 0, sizeof(*this));
+			static_cast<void>(game.read(*relativeToAbsolute<decltype(this)*>(findPattern(game.getImage(), pattern) + offset), this, sizeof(*this)));
+		}
+
+		struct Entry
+		{
 			std::uint64_t index; //0x0000
 			Entry* next; //0x0008
 			std::uint8_t name[1024]; //0x0010
+
 		}; //Size: 0x0410
 		static_assert(sizeof(Entry) == 0x410);
 
-		struct Block {
+		struct Block
+		{
 			Entry* entry[16384]; //0x0000
+
 		}; //Size: 0x20000
 		static_assert(sizeof(Block) == 0x20000);
 
+		auto print(const UEGame& game) noexcept
+		{
+			for (std::size_t i = 0; i < (sizeof(blocks) / sizeof(blocks[0])); i++) {
+
+				std::unique_ptr<Block> block = std::make_unique<decltype(block)::element_type>();
+				if (!game.read(blocks[i], block.get(), sizeof(decltype(block)::element_type)))
+					continue;
+
+				for (std::size_t j = 0; j < (sizeof(block->entry) / sizeof(block->entry[0])); j++) {
+
+					std::unique_ptr<Entry> entry = std::make_unique<decltype(entry)::element_type>();
+					if (!game.read(block->entry[j], entry.get(), sizeof(decltype(entry)::element_type)))
+						continue;
+
+					std::printf("[%p][%08X] %s\n", reinterpret_cast<void*>(block->entry[j]), static_cast<std::uint32_t>(entry->index / 2), entry->name);
+				}
+			}
+		}
+
 		Block* blocks[35]; //0x0000
+
 	}; //Size: 0x0118
 	static_assert(sizeof(UENamePool_4_20_3_0) == 0x118);
 }
@@ -142,36 +213,11 @@ UEEngine::UEEngine(const UEGame& game) noexcept : error{ false }
 				FNameEntry.LenBit = 6;
 				FNameEntry.HeaderSize = 2;
 
-				const auto namepoolPattern = "\x48\x8D\x0D????\xE8????\xC6\x05????\x01\x0F\x10\x03\x4C\x8D\x44\x24\x20\x48\x8B\xC8"; // 48 8D 0D ? ? ? ? E8 ? ? ? ? C6 05 ? ? ? ? 01 0F 10 03 4C 8D 44 24 20 48 8B C8
 				const auto gobjectsPattern = "\x48\x8B\x05????\x48\x8B\x0C\xC8\x48\x8D\x04\xD1\xEB"; // 48 8B 05 ? ? ? ? 48 8B 0C C8 48 8D 04 D1 EB
-	
-				const auto namepoolPtr = relativeToAbsolute<UENamePool_4_27_2_0*>(findPattern(game.getImage(), namepoolPattern) + 3);
 				const auto gobjectsFakeAddy = relativeToAbsolute<std::uintptr_t*>(findPattern(game.getImage(), gobjectsPattern) + 3);
-
-				for (std::uint32_t i = 0; i < namepoolPtr->currentBlock; i++) {
-
-					for (std::uint32_t j = 0; j < namepoolPtr->currentByteCursor;) {
-
-						std::size_t info{};
-						if (!game.read(reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(namepoolPtr->blocks[i]) + j), &info, FNameEntry.HeaderSize))
-							continue;
-
-						auto length = info >> FNameEntry.LenBit;
-						const auto wide = (info >> FNameEntry.WideBit) & 1;
-
-						j += FNameEntry.HeaderSize;
-
-						std::unique_ptr<std::byte[]> name = std::make_unique<decltype(name)::element_type[]>(length + 1);
-						if (!game.read(reinterpret_cast<void*>(reinterpret_cast<std::uintptr_t>(namepoolPtr->blocks[i]) + j), name.get(), length))
-							continue;
-
-						j += static_cast<decltype(j)>(length);
-						if (length % 2)
-							j++;
-
-						std::printf("%s[%zd]\n", reinterpret_cast<const char*>(name.get()), length);
-					}
-				}
+	
+				std::unique_ptr<UENamePool_4_27_2_0> names = std::make_unique<decltype(names)::element_type>(game, "\x48\x8D\x0D????\xE8????\xC6\x05????\x01\x0F\x10\x03\x4C\x8D\x44\x24\x20\x48\x8B\xC8", 3); // 48 8D 0D ? ? ? ? E8 ? ? ? ? C6 05 ? ? ? ? 01 0F 10 03 4C 8D 44 24 20 48 8B C8
+				names->print(game, *this);
 			}
 		}
 		else if (20 <= version[1]) {
@@ -180,32 +226,11 @@ UEEngine::UEEngine(const UEGame& game) noexcept : error{ false }
 				// Game List
 				// 1. Borderlands 3 (Epic Games)
 
-				const auto namepoolPattern = "\x48\x83\xEC\x28\x48\x8B\x05????\x48\x85\xC0\x75?\xB9????\x48\x89\x5C\x24\x20\xE8"; // 48 83 EC 28 48 8B 05 ? ? ? ? 48 85 C0 75 ? B9 ? ? ? ? 48 89 5C 24 20 E8
 				const auto gobjectsPattern = "\x48\x8B\x0D????\x48\x98\x4C\x8B\x04\xD1\x48\x8D\x0C\x40\x49\x8D\x04\xC8\xEB"; // 48 8B 0D ? ? ? ? 48 98 4C 8B 04 D1 48 8D 0C 40 49 8D 04 C8 EB
-			
-				std::unique_ptr<UENamePool_4_20_3_0> namePool = std::make_unique<decltype(namePool)::element_type>();
-				if (!game.read(*relativeToAbsolute<UENamePool_4_20_3_0**>(findPattern(game.getImage(), namepoolPattern) + 7), namePool.get(), sizeof(decltype(namePool)::element_type))) {
-					error = true;
-					return;
-				}
-
 				const auto gobjectsFakeAddy = relativeToAbsolute<std::uintptr_t*>(findPattern(game.getImage(), gobjectsPattern) + 3);
 
-				for (std::size_t i = 0; i < (sizeof(namePool->blocks) / sizeof(namePool->blocks[0])); i++) {
-
-					std::unique_ptr<UENamePool_4_20_3_0::Block> block = std::make_unique<decltype(block)::element_type>();
-					if (!game.read(namePool->blocks[i], block.get(), sizeof(decltype(block)::element_type)))
-						continue;
-
-					for (std::size_t j = 0; j < (sizeof(block->entry) / sizeof(block->entry[0])); j++) {
-
-						std::unique_ptr<UENamePool_4_20_3_0::Entry> entry = std::make_unique<decltype(entry)::element_type>();
-						if (!game.read(block->entry[j], entry.get(), sizeof(decltype(entry)::element_type)))
-							continue;
-
-						std::printf("[%p][%08X] %s\n", reinterpret_cast<void*>(block->entry[j]), static_cast<std::uint32_t>(entry->index / 2), entry->name);
-					}
-				}
+				std::unique_ptr<UENamePool_4_20_3_0> names = std::make_unique<decltype(names)::element_type>(game, "\x48\x83\xEC\x28\x48\x8B\x05????\x48\x85\xC0\x75?\xB9????\x48\x89\x5C\x24\x20\xE8", 7); // 48 83 EC 28 48 8B 05 ? ? ? ? 48 85 C0 75 ? B9 ? ? ? ? 48 89 5C 24 20 E8
+				names->print(game);
 			}
 		}
 	}
